d2bfea5d83c7bc47db7ef387ce74f3940a4ecf14
Jakub Orzyłowski <s21001@pjwstk.edu.pl>
2022-09-13 05:50:52 +0200

    Strona IRW API responsywne w miarę działające API

--- niebiescy_best/js/form_sumit_js.js
+++ niebiescy_best/js/form_sumit_js.js
@@ -6,14 +6,30 @@ function recruit_form_submitted(){
 
 
     let xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://lady-isabell.herokuapp.com/resSociety", true);
+    // xhr.open("POST", "https://lady-isabell.herokuapp.com/resSociety", true);
+
+    xhr.open("POST", "http://0.0.0.0:5000/resSociety", true);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
-    xhr.send(data);
+    xhr.send(JSON.stringify(data));
     xhr.onload = function() {
-        console.log("HELLO")
-        console.log(this.responseText);
-        let responsedata = JSON.parse(this.responseText);
-        console.log(responsedata);
+        if (this.status === 200) {
+            console.log("Sukces");
+            form.remove();
+            let message = document.createElement("h2")
+            message.classList.add("form_title")
+            message.innerText = "Zgłoszenie pomyślnie wysłane";
+            let messagebody = document.createElement("p");
+            messagebody.innerText = "Wkrótce ktoś się z Tobą skontaktuje";
+            let box = document.querySelector('.form_box');
+            box.classList.remove("form_box");
+            box.classList.add("contentBox");
+            box.appendChild(message);
+            box.appendChild(messagebody);
+        } else {
+            let responsedata = JSON.parse(this.responseText);
+            console.log(responsedata);
+            document.querySelector('#form_error_message').innerHTML = "Zgłoszenie zawera błędy";
+        }
     }
 }
--- niebiescy_best/js/photoChanger.js
+++ niebiescy_best/js/photoChanger.js
@@ -31,10 +31,10 @@ function throttle(func, wait) {
 }
 
 function onDocumentLoaded(){
-    // if (isMobile){
-    //     let logo_link = document.getElementById("logo_link");
-    //     logo_link.innerText = "IRP";
-    // }
+    if (isMobile){
+        let logo_link = document.getElementById("logo_link");
+        logo_link.innerText = "IRP";
+    }
     loadSlides();
     document.getElementById("submit_button").addEventListener("click", function(event){
         event.preventDefault();
